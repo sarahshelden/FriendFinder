d6a75ee5582b32cc3a32101890e9f8e4f11b4486
Sarah Shelden <sarah.shelden113@gmail.com>
2018-05-08 10:08:53 -0400

    server

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+
+// uses express server
+var app = express();
+
+
+
+//PORT
+
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// ROUTER
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+// LISTENER
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
+
 
